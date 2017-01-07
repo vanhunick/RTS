dca1c00bfdf21a24ef0943891d41e43b40fab192
Jacob Love <jacobmlove95@gmail.com>
2017-01-07 22:15:10 +1300

    Added ability to drag RTS map

--- public/javascripts/Game.js
+++ public/javascripts/Game.js
@@ -6,16 +6,39 @@ function update() {
 var renderer = PIXI.autoDetectRenderer(1280, 720, { backgroundColor: 0x7eC0ee }); // Sky blue background
 //document.body.appendChild(renderer.view);
 
-console.log("Test "  + document.getElementById("gameDiv"));
-document.getElementById("gameDiv").appendChild(renderer.view);
+console.log('Test '  + document.getElementById('gameDiv'));
+document.getElementById('gameDiv').appendChild(renderer.view);
 
 requestAnimationFrame(update);
 
 var instanceConfig = {
-    mapDataPath:  "resources/test_map.xml",
-    assetsToLoad: ["resources/grass.png"]
+    mapDataPath:  'resources/test_map.xml',
+    assetsToLoad: ['resources/grass.png']
 };
 
 stage = new PIXI.Stage();
 var engine = TRAVISO.getEngineInstance(instanceConfig);
 stage.addChild(engine);
+
+stage.interactive = true;
+stage.on('mousedown', onDragStart);
+stage.on('mouseup',   onDragStop);
+stage.on('mousemove', onDragMove);
+
+function onDragStart(event) {
+    this.data = event.data;
+    this.dragging = true;
+}
+
+function onDragStop() {
+    this.data = null;
+    this.dragging = false;
+}
+
+function onDragMove() {
+    if (this.dragging) {
+        var newPosition = this.data.getLocalPosition(this.parent);
+        this.position.x = newPosition.x;
+        this.position.y = newPosition.y;
+    }
+}
