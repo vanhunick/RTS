69993be3c5ea293d3fcc53716e30acd3af7112da
Nicky van Hulst <nickyvanhulst@gmail.com>
2017-01-08 13:33:07 +1300

    Added a nav bar and slight modifications to css

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 var postgres = require('pg');
-var postgresClient = new postgres.Client('postgres://postgres:admin@localhost:5432/Users');
-postgresClient.connect();
+//var postgresClient = new postgres.Client('postgres://postgres:admin@localhost:5432/Users');
+//postgresClient.connect();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: 'RTS' });
 });
 
 module.exports = router;
